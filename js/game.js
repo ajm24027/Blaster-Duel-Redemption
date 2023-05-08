e46a29e61d1b5133595acd1d358e40c466104b95
Anthony Medina <anthony.john.medina@gmail.com>
2023-05-07 22:12:39 -0400

    Added Duel Page and Layout

--- js/game.js
+++ js/game.js
@@ -1,6 +1,41 @@
-const open = document.getElementById('open')
-const modal_container = document.getElementById('modal_container')
-const close = document.getElementById('close')
+/*----- constants -----*/
+const open = document.querySelector('#open')
+const modal_container = document.querySelector('#modal_container')
+const close = document.querySelector('#close')
+const startBtn = document.querySelector('#start-btn')
+
+/*----- app's state (variables) -----*/
+let round
+let baronBested
+let winner
+let lives
+/*----- cached element references -----*/
+
+/*----- functions -----*/
+const getRndmNums = () => {
+  let randomNums = []
+  for (let i = 0; i < round + 4; i++) {
+    let randomNum = Math.floor(Math.random() * 10) + 1
+    randomNums.push(randomNum)
+  }
+  console.log(randomNums)
+}
+
+const init = () => {
+  round = 1
+  baronBested = 0
+  winner = null
+  render()
+}
+
+const render = () => {
+  renderRound()
+  renderBaron()
+  renderLives()
+  renderReference()
+}
+
+/*----- event listeners -----*/
 
 open.addEventListener('click', () => {
   modal_container.classList.add('show')
@@ -8,4 +43,9 @@ open.addEventListener('click', () => {
 
 close.addEventListener('click', () => {
   modal_container.classList.remove('show')
+})
+
+startBtn.addEventListener('click', () => {
+  window.location.href =
+    '/Users/anthonymedina/SEI-R-4-24/projects/Blaster-Duel-Redemption/duel.html'
 })
